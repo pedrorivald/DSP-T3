6fe528bb5dea14a76b6ee3dcc9a1d8c129abb7d0
Pedro Rivaldo <pedrorivaldev@gmail.com>
2025-02-11 16:17:12 -0300

    name host

--- db/db.py
+++ db/db.py
@@ -1,7 +1,7 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-host = os.getenv("MONGO_HOST", "localhost")
+host = os.getenv("MONGO_HOST", "oficina-mongodb")
 
 MONGO_URI = f"mongodb://{host}:27017/oficina"
 client = AsyncIOMotorClient(MONGO_URI)
